9bb1cdf9c6df2163e96fd25962fb1b5d9e6a3170
thefifthwaffle <honased@gmail.com>
2019-10-28 16:39:14 -0500

    Defined the Palindrome, 1d to 2d, and Prime Methods

--- Chocolatey Interview/Program.cs
+++ Chocolatey Interview/Program.cs
@@ -12,5 +12,32 @@ namespace Chocolatey_Interview
         {
             Console.ReadKey();
         }
+
+        internal static void chkPalindrome(string str)
+        {
+             /* input:-madam; output:-Palindrome
+             * input:-step on no pets;output-Palindrome
+             * input:-book; output-Not Palindrome
+             * 
+             * */
+        }
+
+        internal static void SingleToMulti(int[] array, int row, int column)
+        {
+            /* input: 1,2,3,4,5,6
+             * output: 1 2 3
+             *         4 5 6
+             * */
+        }
+
+        internal static bool FindPrime(int number)
+        {
+            /* input:-20 output:-Not Prime
+             * input:-17 output:-Prime
+             * 
+             * */
+
+            return false;
+        }
     }
 }
