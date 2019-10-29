03da0fcb77794ff3e3c021fb21a7cb5cce62c201
thefifthwaffle <honased@gmail.com>
2019-10-29 13:33:20 -0500

    An empty string is no longer considered a palindrome. Removed testing.

--- Chocolatey Interview/Program.cs
+++ Chocolatey Interview/Program.cs
@@ -11,56 +11,6 @@ namespace Chocolatey_Interview
     {
         static void Main(string[] args)
         {
-            //Tests for chkPalindrome
-
-            //Time test - Result 6 ms
-            var stopwatch = new Stopwatch();
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 99999; i++)
-            {
-                sb.Append("AAAAAAAAA");
-            }
-            String convertedString = sb.ToString();
-            stopwatch.Start();
-            chkPalindrome(convertedString);
-            stopwatch.Stop();
-
-            Console.WriteLine("Time: " + stopwatch.ElapsedMilliseconds);
-
-            //Uppercase/lowerCase test - Good
-            Console.Write("Upper/Lower test [MaDdAm]: ");
-            chkPalindrome("MaDdAm");
-
-            //Even # test - Good
-            Console.Write("Even Test [Maddam]: ");
-            chkPalindrome("Maddam");
-
-            //Odd # test - Good
-            Console.Write("Odd Test [Madam]: ");
-            chkPalindrome("Madam");
-
-            //Check to make sure it detects false palindromes - Good
-            Console.Write("False Test [Hello World]: ");
-            chkPalindrome("Hello World");
-            Console.Write("False Test [Chocolatey]: ");
-            chkPalindrome("Chocolatey");
-
-            //Step on no pets test - Good
-            Console.Write("Regular Test [step on no pets]: ");
-            chkPalindrome("step on no pets");
-
-            //Spacing test - Good
-            Console.Write("Spacing test [a nut for a jar of tuna]: ");
-            chkPalindrome("a nut for a jar of tuna");
-
-            //Empty string test - It returns a palindrome, but does that make sense?
-            Console.Write("Empty string test []: ");
-            chkPalindrome("");
-
-            //Final test - Good
-            Console.Write("Cumulative Test [Al lets Della call Ed Stella]: ");
-            chkPalindrome("Al lets Della call Ed Stella");
-
             Console.ReadKey();
         }
 
@@ -76,13 +26,19 @@ namespace Chocolatey_Interview
             * 
             * */
 
-            //Convert the string to lower so that uppercase doesn't effect check
-            //Also, remove spaces as some palindromes allow for different spacing
+            //Removes spaces as some palindromes allow for different spacing
             //IE: A nut for a jar of tuna
             str = str.Replace(" ", "");
 
             //Get the middle of the string for the loop
             int strLength = str.Length;
+
+            if(strLength == 0)
+            {
+                Console.WriteLine("Not Palindrome");
+                return;
+            }
+
             int mid = strLength / 2;
             
             //This loop will increment i under 2 conditions
